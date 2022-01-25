a054ee60e5ea0a3d56d6c2a28f55548f26d10b2d
anikaweinmann <aweinmann@mundialis.de>
2022-01-25 14:21:00 +0100

    test workflow with NC location possible

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,6 +3,16 @@
 # fail on non-zero return code from a subprocess
 set -e
 
+# download NC test loaction if the test needs the data and run tests
+if [ $1 == "NC" ]
+then
+  g.extension g.download.location
+  g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=/grassdb
+  g.mapset mapset=PERMANENT location=nc_spm_full_v2alpha2 -c
+  g.list all
+fi
+
+# run all tests in folder
 FILENAME=$(basename "$(find . -name *.html -maxdepth 1)")
 ADDON="${FILENAME%%.html}"
 
