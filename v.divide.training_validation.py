6473c7a71b182d814410124b9f25c731387ee5a9
Markus Metz <markus.metz.giswork@gmail.com>
2026-09-11 17:22:46 +0200

    update and add workflows

--- v.divide.training_validation.py
+++ v.divide.training_validation.py
@@ -45,15 +45,19 @@
 # % answer: 30
 # %end
 
-import grass.script as grass
 import os
 import random
 import atexit
+import grass.script as grass
+
 
 newcol = None
 
 
 def cleanup():
+    """
+    Cleanup function to remove temporary data.
+    """
     grass.message(_("Cleaning up..."))
     if newcol:
         columns_existing = grass.vector_columns(options["input"]).keys()
@@ -64,6 +68,9 @@ def cleanup():
 
 
 def extract_data(input, output, cats, value):
+    """
+    Extracts data from input vector map based on category list and writes to output vector map.
+    """
     if len(cats) > 20000:
         newcol = "train_val_%s" % (os.getpid())
         columns_existing = grass.vector_columns(options["input"]).keys()
@@ -96,6 +103,10 @@ def extract_data(input, output, cats, value):
 
 
 def main():
+    """
+    Divide input into training and validation data.
+    """
+
     global newcol
 
     input = options["input"]
